46e440d577c5011f8cf83c64ac78e28bfb8b3dd3
MarchanChou <834157178@qq.com>
2022-04-12 17:19:58 +0800

    init

---
+++ dist/index.de158e3a.js
@@ -0,0 +1,62 @@
+const $lastLi = $('.last');
+const x = localStorage.getItem('x');
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+    {
+        logo: 'A',
+        url: 'https://www.acfun.cn'
+    },
+    {
+        logo: 'B',
+        url: 'https://www.bilibili.com'
+    }, 
+];
+const simplifyUrl = (url)=>{
+    return url.replace('https://', '').replace('http://', '').replace('www.', '').replace(/\/.*/, '');
+};
+const render = ()=>{
+    $('.siteList').find('li:not(.last)').remove() // 除了最后一个全部删除
+    ;
+    hashMap.forEach((node, index)=>{
+        const $li = $(`<li>
+                <a href="${node.url}">
+                    <div class="site">
+                        <div class="logo">${node.logo}</div>
+                        <div class="link">${simplifyUrl(node.url)}</div>
+                        <div class="close">
+                            <svg class="icon" aria-hidden="true">
+                                <use xlink:href="#icon-close"></use>
+                            </svg>
+                        </div>
+                    </div>
+                </a>
+            </li>`).insertBefore($lastLi);
+        $li.on('click', '.close', (e)=>{
+            e.preventDefault();
+            hashMap.splice(index, 1);
+            render();
+        });
+    });
+};
+render();
+$lastLi.on('click', ()=>{
+    let url = window.prompt('添加网址');
+    if (url.indexOf('http') !== 0) url = 'https://' + url;
+    hashMap.push({
+        logo: simplifyUrl(url)[0].toUpperCase(),
+        url: url
+    });
+    render();
+});
+window.onbeforeunload = ()=>{
+    const string = JSON.stringify(hashMap);
+    localStorage.setItem('x', string);
+};
+$(document).on('keypress', (e)=>{
+    const key = e.key;
+    hashMap.forEach((node)=>{
+        if (node.logo.toLowerCase() === key) window.open(node.url, '_self');
+    });
+});
+
+//# sourceMappingURL=index.de158e3a.js.map
